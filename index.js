bf12a733bf663f908823c1ce0619a0bd7f51ebfa
Noiselles <gamer24.com@gmail.com>
2018-01-16 20:16:53 +0200

    123

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 const https = require('https');
 const request = require('request');
@@ -17,6 +9,16 @@ const config = require('./config.json')
 const utf8 = require('utf8')
 const chatId = null;
 const bot = new Telegraf(config.token)
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
 var SettingsObj = {day: true, timing: true}
 
 var ChatsArr = [];
