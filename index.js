f81a23d1f4aa0870a9dce72fb08887a20f98b401
Noiselles <gamer24.com@gmail.com>
2018-01-18 20:56:20 +0200

    123

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
 const https = require('https');
 const request = require('request');
@@ -10,12 +10,12 @@ const utf8 = require('utf8')
 const bot = new Telegraf(config.token)
 const mysql = require('mysql')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // var client = mysql.createConnection({
 //   host: "192.168.1.5",
